4b0c8341ad33c261297f63a96bc9817cb05a84dc
Avyanshgupta18 <avyansh04@gmail.com>
2024-09-28 18:46:17 -0400

    Updated

--- SDLProject/main.cpp
+++ SDLProject/main.cpp
@@ -6,7 +6,15 @@
  *
  * @copyright NYU Tandon (c) 2024
  */
-
+/**
+* Author: Avyansh Gupta
+* Assignment: Simple 2D Scene
+* Date due: 2023-09-20, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
 
 #define GL_SILENCE_DEPRECATION
 #define STB_IMAGE_IMPLEMENTATION
